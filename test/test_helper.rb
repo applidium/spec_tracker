4e234a504796ec6dc5b6d79128811b6fee8d7768
Joanna Vigné <joanna.vigne@fabernovel.com>
2019-01-20 18:37:50 +0100

    configure minitest reporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "spec_tracker"
 
 require "minitest/autorun"
+require "minitest/reporters"
+
+Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::JUnitReporter.new]
