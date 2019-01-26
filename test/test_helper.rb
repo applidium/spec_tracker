f4970dbabccfee370f17c7710ac30aca255025f8
Joanna Vigné <joanna.vigne@fabernovel.com>
2019-01-27 00:00:06 +0100

    use active support testing declarative extension

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,13 @@
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "spec_tracker"
 require 'byebug'
+require 'active_support/testing/declarative'
 
 require "minitest/autorun"
 require "minitest/reporters"
 
 Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new, Minitest::Reporters::JUnitReporter.new]
+
+class BaseTest < Minitest::Test
+  extend ActiveSupport::Testing::Declarative
+end
